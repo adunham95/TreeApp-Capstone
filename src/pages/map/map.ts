b4c610bae7545655e1da06198e6dcbbf7c7aefd9
adunham95 <adunham95@gmail.com>
2017-03-23 09:43:26 -0400

    Added current location Issue #5

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,3 +1,4 @@
+import { Geolocation } from '@ionic-native/geolocation';
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import {NavController, Platform, ModalController} from 'ionic-angular';
 import {Http} from "@angular/http";
@@ -18,9 +19,26 @@ export class Map {
   @ViewChild('map') mapElement: ElementRef;
       map: any;
 
-  constructor(public navCtrl: NavController, public platform: Platform, private http:Http,public modalCtrl: ModalController) {
+  constructor(public navCtrl: NavController, public platform: Platform, private http:Http,public modalCtrl: ModalController, private geolocation: Geolocation) {
     http.get('http://adriandunham.com/treesFULL.json')
       .subscribe(res => this.forest = res.json());
+
+
+    geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+    let watch = geolocation.watchPosition();
+    watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      this.lat = data.coords.latitude;
+      this.lng = data.coords.longitude;
+    });
+
+
   }
 
   openModal(characterNum) {
