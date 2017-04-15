0f57889af883a55be39eb1c0d9b5edb55dbbae01
adunham95 <Apd@21295>
2017-04-14 20:36:03 -0400

    Added singleTree page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export {Home} from './home/home';
 export {Trees} from './trees/trees';
 export {Map} from './map/map';
 export {startPage} from './startPage/startPage';
+export {singleTree} from './singleTree/singleTree';
---
+++ src/pages/singleTree/singleTree.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'singleTree',
+  templateUrl: 'src/pages/singleTree/singleTree.html'
+})
+export class singleTree {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  description = " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+}
